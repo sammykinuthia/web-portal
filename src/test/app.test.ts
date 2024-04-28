6ef83fa46574274b50cd71b5921059e8ea9d4711
sammykinuthia <kinuthiasamuel05@gmail.com>
2024-04-28 20:59:57 +0300

    added test

---
+++ src/test/app.test.ts
@@ -0,0 +1,24 @@
+// Imports
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+// To Test
+import App from '../App';
+
+// Tests
+describe('Renders main page correctly', async () => {
+    /**
+     * Passes - shows title correctly
+     */
+    it('Should render the page correctly', async () => {
+        // Setup
+        // await render(<App />);
+        // const h1 = await screen.queryByText('Vite + React');
+
+        // Post Expectations
+        // expect(h1).not.toBeNull();
+        expect(1).equal(1)
+    });
+
+   
+});

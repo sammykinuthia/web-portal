f44d4d6c87cee1d00eebff93ccb98efe3dc1c547
sammykinuthia <kinuthiasamuel05@gmail.com>
2024-05-13 12:51:45 +0300

    update

--- src/pages/Thankyou.tsx
+++ src/pages/Thankyou.tsx
@@ -1,7 +1,5 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const ThankyouPage = () => {
     const navigate = useNavigate()
